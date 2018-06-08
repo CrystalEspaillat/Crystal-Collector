5b3d538609881db8457b2bc10d8c3cc78313a710
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-06-08 17:45:26 -0400

    Finished HTML and CSS layout for game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Function that randomly assigns each crystal a value
+// Function that randomly chooses a goal number.
+// Call functions on page load.
+
+//When user clicks a crystal ...
+// if value + score is less than or equal to goal number 
+// then animate crystal falling into basket
+// change score value
+
+//if value + score is greater than goal number
+// add a loss
+// animate crystal to pulse.
